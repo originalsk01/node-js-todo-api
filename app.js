0c9b603296f158b1c8324f256054ee8b0bb32c80
orginalsk01 <sipssman@gmail.com>
2023-06-26 18:50:04 +0200

    Add Todo routes and enable cors middleware

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 
 const db = require("./data/database");
+const todosRouter = require("./routes/todos.routes");
+const enableCors = require("./middlewares/cors");
 
 const app = express();
+
+app.use(enableCors);
+app.use(express.json());
+
+app.use("/todos", todosRouter);
 
 app.use(function (error, req, res, next) {
   res.status(500).json({
